3af75afab8f74020eb8886a533edaa4031c39b50
gennabarge <gennabarge@172.16.21.52>
2018-09-13 08:21:34 -0400

    Pseudo Code

---
+++ src/U1L02MakingChange.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+//Genna Barge
+//U1L02 Pseudo Code
+public class U1L02MakingChange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//nickels = 0
+		//String input = JOptionPane.showInputDialog("Enter an amount of money ex: 3.12");
+		//int money = Integer.parseInt(input);
+		//while money > .05{
+		//money -= .05
+		//nickels+=1
+		//}
+		//pennies = money * 10
+		//JOptionPane.showMessageDialog(null, "you need " +
+		// nickels + " nickels, and " + pennies) 
+
+	}
+
+}
